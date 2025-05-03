76d786bc37143e4b61db8e17dd073c08907ad0b8
Siim Türk <siim.turk@askend.com>
2025-05-03 10:43:19 +0300

    Added value input box with conditions and modifying modal closing

--- src/app/add-filter/add-filter.component.ts
+++ src/app/add-filter/add-filter.component.ts
@@ -1,22 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ApiService} from '../shared/api.service';
 import {FilterRequest} from '../shared/filterrequest';
 import {Type} from '../shared/type';
 import {Condition} from '../shared/condition';
 import { catchError, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import {CriteriaRequest} from '../shared/criteriarequest';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-add-filter',
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf,
+    FormsModule
   ],
   templateUrl: './add-filter.component.html',
   styleUrl: './add-filter.component.css'
 })
-export class AddFilterComponent implements OnInit {
+export class AddFilterComponent implements OnInit  {
   model: FilterRequest = {
     name: '',
     criteriaRequests: []
@@ -26,6 +29,21 @@ export class AddFilterComponent implements OnInit {
   criterias: CriteriaRequest[] = [];
   defaultType: string = '';
   defaultCondition: string = '';
+
+  @ViewChild('filterModal') filterModalRef!: ElementRef;
+
+  ngAfterViewInit() {
+    const modalEl = this.filterModalRef.nativeElement;
+
+    modalEl.addEventListener('hidden.bs.modal', () => {
+      this.onModalClosed();
+    });
+  }
+
+  onModalClosed() {
+    this.deleteFilter();
+    console.log('Modal is closed');
+  }
 
   ngOnInit() {
     this.loadInitialData();
@@ -81,8 +99,9 @@ export class AddFilterComponent implements OnInit {
     });
   }
 
-  onTypeChange(event: any) {
+  onTypeChange(event: any, criteria: CriteriaRequest) {
     this.findComparingConditionByType(event.target.value);
+    criteria.value = '';
   }
 
   private initCriteria(type: string, condition:string): void {
@@ -98,5 +117,25 @@ export class AddFilterComponent implements OnInit {
   private setDefaultValues(type: string, condition: string) {
     this.defaultType = type;
     this.defaultCondition = condition;
+  }
+
+  sendRequest(): void {
+  }
+
+  showAmount(criteria: CriteriaRequest) {
+    return criteria.type == 'Amount';
+  }
+
+  showTitle(criteria: CriteriaRequest) {
+    return criteria.type == 'Title';
+  }
+
+  showDate(criteria: CriteriaRequest) {
+    return criteria.type == 'Date';
+  }
+
+  deleteFilter() {
+    this.criterias = [];
+    this.model.name = '';
   }
 }
